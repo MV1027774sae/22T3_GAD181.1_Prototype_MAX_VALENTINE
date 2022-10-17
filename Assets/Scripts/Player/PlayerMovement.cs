b3c86899c969a9743bfbcfd5d1df1ff5a8cdb25b
Max Valentine <107592271+1027774sae@users.noreply.github.com>
2022-10-18 00:12:12 +1100

    MV Particles

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public ParticleSystem movementSparks;
+    public ParticleSystem jumpSparks;
     public Rigidbody2D rigidbody2d;
     private BoxCollider2D boxCollider2d;
 
@@ -22,11 +24,26 @@ public class PlayerMovement : MonoBehaviour
         {
             float jumpVelocity = 19f;
             rigidbody2d.velocity = Vector2.up * jumpVelocity;
+            CreateJumpSparks();
+        }
+        if (IsGrounded())
+        {
+            CreateMovementSparks();
         }
     }
     private bool IsGrounded()
     {
         RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, 0.15f, layerMask);
         return raycastHit2d.collider != null;
+    }
+
+    public void CreateMovementSparks()
+    {
+        movementSparks.Play();
+    }
+
+    public void CreateJumpSparks()
+    {
+        jumpSparks.Play();
     }
 }
